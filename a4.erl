730a4d8d8d2626a3353d26b3b0f464fc2bd69813
Patrick Skinner <patrickskinner@outlook.com>
2017-10-14 14:49:58 +1300

    Added auction list and auction add/remove methods.

--- a4.erl
+++ a4.erl
@@ -1,18 +1,26 @@
 -module(a4).
--export([a4/0, receiver/1]).
+-export([a4/0, receiver/2]).
 
 a4()->
-	MR = spawn(?MODULE, receiver, [[]]),
+	MR = spawn(?MODULE, receiver, [[],[]]),
 	register(service, MR).
 	
-receiver(Clients)->
+receiver(Clients, Auctions)->
 	receive
-		{client_join, Msg} ->
+		{client_add, Msg} ->
 			NewClients = [{Msg} | Clients],
-			receiver(NewClients);
+			receiver(NewClients, Auctions);
 			
-		{client_leave, Msg} ->
+		{client_remove, Msg} ->
 			NewClients = lists:keydelete(Msg, 1, Clients),
-			receiver(NewClients)
+			receiver(NewClients, Auctions);
+			
+		{auction_add, Msg} ->
+			NewAuctions = [{Msg} | Auctions],
+			receiver(Clients, NewAuctions);
+			
+		{auction_remove, Msg} ->
+			NewAuctions = lists:keydelete(Msg, 1, Auctions),
+			receiver(Clients, NewAuctions)
 	end.
 	
